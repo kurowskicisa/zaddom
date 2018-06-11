f4acc06e7921caeb1491330cb9001a14a21a824b
kurowskicisa <kurowskic.isa@gmail.com>
2018-06-11 23:09:15 +0200

    Drobne zmiany

--- Cards/src/Cards.java
+++ Cards/src/Cards.java
@@ -20,26 +20,25 @@ public class Cards {
         System.out.println( mojaKarta );
 
         Scanner readKolor = new Scanner(System.in);
-        Scanner readFigua = new Scanner(System.in);
+        Ranks inputRanks;
+        try (Scanner readFigua = new Scanner(System.in)) {
 
 
+            System.out.println("Podaj figura ( ACE lub KING lub QUEEN lub JACK ): ");
 
-////////
-        System.out.println("Podaj figura ( ACE lub KING lub QUEEN lub JACK ): " );
-// do
-        Ranks inputRanks = Ranks.valueOf(readFigua.next().toUpperCase());
-// whie brak błędu
+            inputRanks = Ranks.valueOf(readFigua.next().toUpperCase());
+        }
+
 
         karta.setFigura(inputRanks);
-////////
+
 
         System.out.println("Podaj kolor ( CLUBS lub DIAMONDS lub HEARTS lub SPADES ): ");
-// do
         Suits inputSuits = Suits.valueOf(readKolor.next().toUpperCase());
-// while brak błędu
 
         karta.setKolor(inputSuits);
-////////
+
+
 
         mojaKarta = karta.GetDescription();
 
